1d1fd5f6619f02b68208094874996712138eea2b
Strilanc <craig.gidney@gmail.com>
2012-03-03 16:47:54 -0400

    Added MrBeetAI - Wander until see character or hear roar then roar and head towards character

---
+++ Assets/Mr_Beet_AI.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Mr_Beet_AI : MonoBehaviour {
+	Vector3? dst = null;
+	float? dstTimeout = null;
+	public AudioClip RoarSound;
+	private bool roarActivated = false;
+	private const float RUN_SPEED = 2.0f;
+	private const float WALK_SPEED = 0.01f;
+	private const float TURN_SPEED = 0.5f;
+	private const float SEARCH_RADIUS = 5.0f;
+	private const float ROAR_RADIUS = 20.0f;
+    public AnimationClip run_clip;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	private void WalkTorwardDst() {
+		if (dst == null) return;
+		
+		var d = dst.Value - transform.position;
+		if (d.magnitude > 0) {
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(d), TURN_SPEED*Time.deltaTime*360);
+		}
+        
+		var speed = roarActivated ? RUN_SPEED : WALK_SPEED;
+		var dist = Time.deltaTime * speed * Mathf.Max(0, Vector3.Dot(d, transform.forward));
+		if (d.magnitude < dist) {
+			transform.position = dst.Value;
+			dst = null;
+		} else {
+			transform.position += dist * transform.forward;
+		}
+		
+		if (dstTimeout != null) {
+			dstTimeout -= Time.deltaTime;
+			if (dstTimeout.Value <= 0) {
+				dst = null;
+				dstTimeout = null;
+				roarActivated = false;
+			}
+		}
+	}
+	
+	private void Roar(Vector3 target) {
+		if (this.roarActivated) return;
+		this.roarActivated = true;
+		
+		//spread roar
+		this.audio.PlayOneShot(this.RoarSound);
+		var nearbyOthers = Physics.OverlapSphere(this.transform.position, ROAR_RADIUS)
+			.Select(e => e.gameObject.GetComponent<Mr_Beet_AI>())
+			.Where(e => e != null);
+		foreach (var e in nearbyOthers)
+			e.Roar(target);
+		
+		//head towards target
+		dst = target;
+		dstTimeout = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.animation.Blend(run_clip.name, 1);
+		WalkTorwardDst();
+		
+		if (dst != null) return;
+		
+		var nearbyHero = Physics.OverlapSphere(this.transform.position, SEARCH_RADIUS)
+			.Where(e => e.gameObject.GetComponent<FaceMoveControl>() != null)
+			.SingleOrDefault();
+		if (nearbyHero != null) {
+			Roar(nearbyHero.transform.position);
+		} else {
+			var halfLife = 0.1f;
+			dst = this.transform.position + new Vector3(Random.Range (-100, 100),0,Random.Range (-100,100));
+			dstTimeout = halfLife + Random.Range(0.0f, halfLife);
+		}
+	}
+}
