2801d3e67ec7bb71dd8158c29ddc5e63630b71a3
harrison <notallama@gmail.com>
2012-03-04 10:19:43 -0400

    changes

--- Assets/Scripts/Bottle.cs
+++ Assets/Scripts/Bottle.cs
@@ -6,7 +6,7 @@ public class Bottle : MonoBehaviour {
     void OnTriggerEnter(Collider c) {
         var go = c.gameObject;
         if (go.name != "PlayerHitbox") return;
-        GameGUI.instance.healthJars++;
+        GameGUI.instance.AddBottle();
         Destroy(gameObject);
     }
 }
